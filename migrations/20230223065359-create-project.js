fe2fabd03ecc30474648ecf3aa13188229d9b7cb
Pablo Villedae <pablov.e12345@gmail.com>
2023-02-22 22:54:17 -0800

    add project model

---
+++ migrations/20230223065359-create-project.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Projects', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      profileId: {
+        type: Sequelize.INTEGER
+      },
+      github: {
+        type: Sequelize.STRING
+      },
+      app: {
+        type: Sequelize.STRING
+      },
+      picture: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Projects');
+  }
+};
